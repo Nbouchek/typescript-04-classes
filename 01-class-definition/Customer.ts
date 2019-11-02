27fb38bbdcdf29b831df21594f61983f71cd0456
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 09:11:45 +0000

    Define a class

---
+++ 01-class-definition/Customer.ts
@@ -0,0 +1,14 @@
+// crate a class
+class Customer {
+  firstName: string;
+  lastName: string;
+}
+
+// let's create an instance
+let myCustomer = new Customer();
+myCustomer.firstName = "Nacer";
+myCustomer.lastName = "Bouchekhima";
+
+console.log(myCustomer.firstName)
+console.log(myCustomer.lastName)
+console.log(myCustomer)
