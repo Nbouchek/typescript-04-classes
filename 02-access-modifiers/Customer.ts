e92278a6d70b7dc046e6ff0c1b033f7b6c963379
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 09:52:18 +0000

    Introducing setters and getters

--- 02-access-modifiers/Customer.ts
+++ 02-access-modifiers/Customer.ts
@@ -1,17 +1,33 @@
 // crate a class
 class Customer {
-  private firstName: string;
-  private lastName: string;
+    private _firstName: string;
+    private _lastName: string;
 
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+    constructor(firstName: string, lastName: string) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    get firstName(): string {
+        return this._firstName;
+    }
+
+    set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    get lastName(): string {
+        return this._lastName;
+    }
+
+    set lastName(value: string) {
+        this._lastName = value;
+    }
 }
 
 // let's create an instance
 let myCustomer = new Customer("Nacer", "Bouchekhima");
 
-console.log(myCustomer.firstName)
-console.log(myCustomer.lastName)
-console.log(myCustomer)
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
+console.log(myCustomer);
