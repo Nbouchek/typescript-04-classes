0da4d0a9dcf11f206c0088aaa63a9f617c7511c5
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 09:17:36 +0000

    Introduce a constructor

--- 01-class-definition/Customer.ts
+++ 01-class-definition/Customer.ts
@@ -2,12 +2,15 @@
 class Customer {
   firstName: string;
   lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
 
 // let's create an instance
-let myCustomer = new Customer();
-myCustomer.firstName = "Nacer";
-myCustomer.lastName = "Bouchekhima";
+let myCustomer = new Customer("Nacer", "Bouchekhima");
 
 console.log(myCustomer.firstName)
 console.log(myCustomer.lastName)
