a8a77f63c7c85e4c169816d8b20246c22aaa70b0
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-02 11:12:57 +0000

    Introducing imports

---
+++ 05-modules/Customer.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// create a class
+// generatte tsconfig using:
+//   tsc --build --clean
+//   tsc --init
+//   tsc
+var Customer = /** @class */ (function () {
+    function Customer(_firstName, _lastName) {
+        this._firstName = _firstName;
+        this._lastName = _lastName;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Customer;
+}());
+exports.Customer = Customer;
---
+++ 05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from "./Customer";
+// let's create an instance
+let myCustomer = new Customer("Nacer", "Bouchekhima");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
+console.log(myCustomer);
